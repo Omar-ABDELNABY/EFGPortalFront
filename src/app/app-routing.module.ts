f2fc4aaf679dfede7d63dd838eb8a56f58aa6cba
omar <timonquint@yahoo.com>
2019-06-02 12:16:12 +0200

    firstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {RegisterationComponent} from "./registeration/registeration.component";
+import {LoginComponent} from "./login/login.component";
+import {IndexComponent} from "./index/index.component";
+
+
+const routes: Routes = [
+  { path: "", redirectTo: "Register", pathMatch: "full" },
+  { path: "Register", component:RegisterationComponent},
+  {path:"portal",component:IndexComponent,
+  children: [
+    { path: "", redirectTo: "x", pathMatch: "full" },
+    { path: "x", component: RegisterationComponent }
+  ]},
+  {path:"login",component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
